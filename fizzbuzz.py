fb9a7b5562081fafb8e9d35849c7ab96b80c59a5
Emmanuel Ojo <emmanuel.ojo97@gmail.com>
2021-01-28 20:07:35 +0000

    fizzbuzz complete

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,2 +1,21 @@
+
+
+def multiple_3_fizz(n):
+  return n % 3 == 0
+
+def multiple_5_fizz(n):
+  return n % 5 == 0
+
+def multiple_15_fizz(n):
+  return n % 15 == 0
+
+def to_fizzbuzz(n):
+  return 'FizzBuzz' if multiple_15_fizz(n) else 'Fizz' if multiple_3_fizz(n) else 'Buzz' if multiple_5_fizz(n) else n
+
+
+
 def fizzbuzz(n):
-    pass
+    list = []
+    for i in range(1, n+1):
+        list.append(to_fizzbuzz(i))
+    return list
